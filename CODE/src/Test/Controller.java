2029869b5ab0700189cd63aef5f6bce2ce297f0d
Marthon <marcibaumel@gmail.com>
2020-10-01 22:03:02 +0200

    rename

--- CODE/src/Test/Controller.java
+++ CODE/src/Test/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package Test;
 
 public class Controller {
 }
--- CODE/src/Test/Main.java
+++ CODE/src/Test/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package Test;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
